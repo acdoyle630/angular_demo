b52045b80f0dfd79105d5174d069cb9ea08d0f1e
Adam <acdoyle630@gmail.com>
2017-05-05 11:02:03 -1000

    step2_server

--- server.js
+++ server.js
@@ -3,5 +3,14 @@
 
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 9000
 
 app.use('/public', express.static('/public'));
+
+app.get('/', (req,res) =>{
+  res.sendFile('index.html', { root: './'});
+});
+
+app.listen(PORT, () =>{
+  console.log(`listening on port: ${PORT}`);
+});
