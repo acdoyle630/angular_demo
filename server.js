78d0e64dd52c95dfd5d0e553be46992be02d07cb
Adam <acdoyle630@gmail.com>
2017-05-05 11:13:01 -1000

    remove the nowe ignored directory node_modules'

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 9000
+const PORT = process.env.PORT || 9000;
 
 app.use('/public', express.static('/public'));
 
